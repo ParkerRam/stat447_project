0f9b5349365c7ea1af0ac4466a310b355e33e31a
ParkerRam <parker.ram97@gmail.com>
2021-03-03 16:01:44 -0800

    Typo againclear

--- modelling.py
+++ modelling.py
@@ -2,5 +2,5 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df_train = pd.read_csv('data/train.pkl', index_col=0)
-df_test = pd.read_csv('data/test.pkl', index_col=0)
+df_train = pd.read_pickle('data/train.pkl')
+df_test = pd.read_pickle('data/test.pkl')
