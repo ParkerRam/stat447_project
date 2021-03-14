453f473955b11ffdec80f2ef2ad5f73b9e21c243
rroxanne <roxannejoier@gmail.com>
2021-03-13 20:46:23 -0800

    got rid of k for oversampling. Used difference between covid counts and max count (for labels) instead to calculate num of duplicates

--- modelling.py
+++ modelling.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+import math
 from sklearn.metrics import confusion_matrix
 from sklearn.linear_model import LogisticRegression
+# import statsmodels.api as sm
 
 df_train = pd.read_pickle('data/train.pkl')
 df_test = pd.read_pickle('data/test.pkl')
@@ -13,28 +15,34 @@ def separateXandY(df):
     y = df[['label4']]
     return x, y
 
-# duplicates covid 19 rows in given training dataset k times
-def oversampleCovid(df_train, k):
+# duplicates covid 19 rows in given training dataset based on max count for labels
+# covid has 50 samples, and maxCount = 2350 (bacteria)
+def oversampleCovid(df_train):
     classes = df_train['label4'].unique()
+    covid_df = df_train.loc[df_train['label4'] == "COVID-19"]
+    covidCount = len(covid_df)
+    maxCount = 0
+
     for label in classes:
         count = len(df_train.loc[df_train['label4'] == label])
         print(label + " has num of rows: " + str(count))
+        if count > maxCount:
+            maxCount = count
+    k = math.floor(((maxCount - covidCount) / covidCount) - 1)
 
-    # oversample covid-19 (covid has 50, while healthy and other vir has ~1345 and bacteria as 2530)
-    covid_df = df_train.loc[df_train['label4'] == "COVID-19"]
-
+    print("Oversampling by duplicating " + str(k) + " times: ")
     frames = [df_train]
     # duplicate covid rows k times
     for i in range(1, k):
         frames.append(covid_df)
     df_oversampled_train = pd.concat(frames)
     covid_df = df_oversampled_train.loc[df_oversampled_train['label4'] == "COVID-19"]
-    print("\nThere are now " + str(len(covid_df)) + " rows with covid-19 labels. (done oversampling).")
+    print("-> There are now " + str(len(covid_df)) + " rows with covid-19 labels. (done oversampling).")
     return df_oversampled_train
 
-# fit logit model (and oversampling that duplicated covid data k num of times) and predict
-def fitLogitReg(df_train, df_test, k):
-    df_oversampled_train = oversampleCovid(df_train, k)
+# fit logit model with oversampling and predict
+def fitLogitReg(df_train, df_test):
+    df_oversampled_train = oversampleCovid(df_train)
     train = separateXandY(df_oversampled_train)
     test = separateXandY(df_test)
     x_train = train[0]
@@ -52,8 +60,18 @@ def fitLogitReg(df_train, df_test, k):
     cfmatrix = np.array(confusion_matrix(y_test, pred, labels=classes))
     print(pd.DataFrame(cfmatrix, index=classes, columns=classes))
 
+def findLogitRegSubset(df_train):
+    df_oversampled_train = oversampleCovid(df_train)
+    train = separateXandY(df_oversampled_train)
+    x_train = train[0]
+    y_train = train[1]
+    model = sm.mnlogit(endog=y_train, exog=x_train).fit()
+    print(model.summary())
+
+
 # multinomial logistic regression with all features
-print("Logit with oversampling (duplicated 5 times)")
-fitLogitReg(df_train, df_test, 5)
+print("Logit with oversampling - used proportion of difference between max and covid count")
+fitLogitReg(df_train, df_test)
 
 # TODO: fit logit reg w/ subset of features
+# subset = findLogitRegSubset(df_train)
