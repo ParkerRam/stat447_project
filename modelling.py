07f72f8118663f135f53af11d0ee0640dd75ec2a
Jillian Chiam <39228399+jillianchiam@users.noreply.github.com>
2021-03-14 01:00:28 -0800

    Added Random Forest

--- modelling.py
+++ modelling.py
@@ -4,6 +4,8 @@ import math
 from sklearn.metrics import confusion_matrix
 from sklearn.linear_model import LogisticRegression
 # import statsmodels.api as sm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
 
 df_train = pd.read_pickle('data/train.pkl')
 df_test = pd.read_pickle('data/test.pkl')
@@ -74,3 +76,58 @@ fitLogitReg(df_train, df_test)
 
 # TODO: fit logit reg w/ subset of features
 # subset = findLogitRegSubset(df_train)
+
+
+def fitRandomForest(df_train, df_test):
+    df_oversampled_train = oversampleCovid(df_train)
+    train = separateXandY(df_oversampled_train)
+    test = separateXandY(df_test)
+    x_train = train[0]
+    y_train = train[1]
+    x_test = test[0]
+    y_test = test[1]
+
+    classes = y_train['label4'].unique()
+
+    y_train = y_train.values.ravel()
+
+    # create random forest 
+    #rf = RandomForestClassifier()
+    rf = RandomForestClassifier(n_estimators= 1400, max_depth= 220, max_features='auto')
+    rf.fit(x_train, y_train)
+
+    #predict
+    rf_predict = rf.predict(x_test)
+
+    # gives me warnings of ValueError: multiclass format is not supported
+    #rf_cv_score = cross_val_score(rf, x_train, y_train, cv=10, scoring='roc_auc')
+
+    rf_matrix = (confusion_matrix(y_test, rf_predict, labels=classes))
+
+    print("Random Forest Confusion Matrix")
+    print(pd.DataFrame(rf_matrix, index=classes, columns=classes))
+    
+fitRandomForest(df_train, df_test)
+
+
+# Find best subset through feature selection
+# These were the features it picked: 
+# 'aboveMedianVar', 'belowMedianVar', 'numOfDarkest', 'numOfLightest',
+# 'x2bar', 'x2ybr', 'xy2br'
+def randomForestFeatures(df_train):
+    df_oversampled_train = oversampleCovid(df_train)
+    train = separateXandY(df_oversampled_train)
+    x_train = train[0]
+    y_train = train[1]
+
+    y_train = y_train.values.ravel()
+
+    rf_select = SelectFromModel(RandomForestClassifier(n_estimators=100))
+    rf_select.fit(x_train, y_train)
+
+    ## To see which features are important on fitted model 
+    rf_select.get_support()
+    selected_feat= x_train.columns[(rf_select.get_support())]
+    len(selected_feat)
+    print(selected_feat)
+
